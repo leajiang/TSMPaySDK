501b107838adab621067ef12dc32980db82a9738
649498510@qq.com <649498510@qq.com>
2020-04-03 14:38:49 +0800

    1.0.1
    1.0.1

--- TSMPaySDK.framework/Headers/HYAlipayManager.h
+++ TSMPaySDK.framework/Headers/HYAlipayManager.h
@@ -18,39 +18,33 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (instancetype)shareManager;
 
-    // 重要说明
-    // 这里只是为了方便直接向商户展示支付宝的整个支付流程；所以Demo中加签过程直接放在客户端完成；
-    // 真实App里，privateKey等数据严禁放在客户端，加签过程务必要放在服务端完成；
-    // 防止商户私密数据泄露，造成不必要的资金损失，及面临各种安全风险；
-    /*============================================================================*/
-    /*=======================需要填写商户app申请的===================================*/
-    /*============================================================================*/
-    //NSString *appID = @"";
-    
-    // 如下私钥，rsa2PrivateKey 或者 rsaPrivateKey 只需要填入一个
-    // 如果商户两个都设置了，优先使用 rsa2PrivateKey
-    // rsa2PrivateKey 可以保证商户交易在更加安全的环境下进行，建议使用 rsa2PrivateKey
-    // 获取 rsa2PrivateKey，建议使用支付宝提供的公私钥生成工具生成，
-    // 工具地址：https://doc.open.alipay.com/docs/doc.htm?treeId=291&articleId=106097&docType=1
-    // NSString *rsa2PrivateKey = @"";
-    // NSString *rsaPrivateKey = @"";
-    /*============================================================================*/
-    /*============================================================================*/
-    /*============================================================================*/
+/// SDK初始化
++ (void)registerApp:(NSString *)appKey secretKey:(NSString *)secretKey;
 
 /// 支付宝支付方法
-/// @param appID 商户app申请的 appID
-/// @param rsa2PrivateKey 如上
-/// @param rsaPrivateKey 如上
-/// @param commodityTitle 商品名字
-/// @param commoditySubject 商品详细内容
-/// @param tradeNO 商品订单号
-/// @param totalAmount 商品金额
-/// @param appScheme 应用注册scheme,在AliSDKDemo-Info.plist定义URL types
-- (void)startAliPay:(NSString *)appID rsa2PrivateKey:(NSString *)rsa2PrivateKey rsaPrivateKey:(NSString *)rsaPrivateKey commodityTitle:(NSString *)commodityTitle commoditySubject:(NSString *)commoditySubject tradeNO:(NSString *)tradeNO totalAmount:(CGFloat)totalAmount appScheme:(NSString *)appScheme;
+/// @param userId 用户ID（暂时直传）
+/// @param outTradeNo 业务订单号（业务请求方系统内单号）
+/// @param tradeAmount 交易金额
+/// @param subject 订单标题
+/// @param notifyUrl 接入商后台结果通知地址
+/// @param appScheme 应用注册scheme,在Info.plist定义URL types
+/// @param completionHandler 支付后回调（resultStatus=9000时支付成功，其他失败并返回错误提示）
+- (void)startAliPayByUserId:(NSString *)userId outTradeNo:(NSString *)outTradeNo tradeAmount:(CGFloat)tradeAmount subject:(NSString *)subject notifyUrl:(NSString *)notifyUrl appScheme:(NSString *)appScheme completionHandler:(void(^)(id result))completionHandler;
+
+/// 支付状态查询(SDK只能查询一天以内产生的订单)
+/// @param outTradeNo 业务订单号(接入商系统)
+/// @param callBack 支付状态
+/// OPEN:初始状态(创建时状态，待支付)
+/// PAYING:支付中
+/// SUCCESS:成功
+/// CLOSED:已关闭
+/// INVALID:无效订单(超时或取消等其他)
+/// 返回空则请求失败
+- (void)orderStatusByoutTradeNo:(NSString *)outTradeNo callBack:(void(^)(NSString *status))callBack;
 
 /// 支付跳转支付宝钱包进行支付，处理支付结果
 -(void)processOrderWithPaymentResult:(NSURL *)url;
+
 @end
 
 NS_ASSUME_NONNULL_END
